20cd1255da0fa41606f43fec23a9e56badab3a47
Kacper Wojciechowski <k.woj.coding@gmail.com>
2022-09-05 13:31:18 +0200

    smart pot init

---
+++ Smart_pot/ESP8266_wifi.ino
@@ -0,0 +1,99 @@
+#include "ThingsBoard.h"
+
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+
+
+#define WIFI_AP             "iPhone (Klaudia)"
+#define WIFI_PASSWORD       "ziumzium1"
+
+// See https://thingsboard.io/docs/getting-started-guides/helloworld/
+// to understand how to obtain an access token
+#define TOKEN               "TGpObTTG5gXsdRphwTQF"
+#define THINGSBOARD_SERVER  "demo.thingsboard.io"
+
+// Baud rate for debug serial
+#define SERIAL_DEBUG_BAUD   115200
+#define SERIAL_MEASURE_BAUD 115200
+
+// Initialize ThingsBoard client
+WiFiClient espClient;
+// Initialize ThingsBoard instance
+ThingsBoard tb(espClient);
+// the Wifi radio's status
+int status = WL_IDLE_STATUS;
+
+char light_c;
+char humidity_c;
+
+void setup() {
+  // initialize serial for debugging
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+  InitWiFi();
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnect();
+  }
+
+  if (!tb.connected()) {
+    // Connect to the ThingsBoard
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+    Serial.println("Connected successfully");
+  }
+
+  if(Serial.available() >= 2)
+  {
+    light_c = Serial.read();
+    humidity_c = Serial.read();
+
+    Serial.println(light_c);
+    Serial.println(humidity_c);
+
+    Serial.println("Sending data...");
+
+    // Uploads new telemetry to ThingsBoard using MQTT.
+    // See https://thingsboard.io/docs/reference/mqtt-api/#telemetry-upload-api
+    // for more details
+
+    tb.sendTelemetryInt("light", (int)light_c);
+    tb.sendTelemetryInt("humidity", (int)humidity_c);
+
+    tb.loop();
+  }
+}
+
+void InitWiFi()
+{
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+
+  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("Connected to AP");
+  }
+}
---
+++ Smart_pot/Nano_Every_measure.ino
@@ -0,0 +1,47 @@
+int lightPin = A1;
+int humidityPin = A6;
+
+int light;
+int humidity;
+int val;
+
+void setup()
+{
+    pinMode(lightPin, INPUT);
+    pinMode(humidityPin, INPUT);
+
+    Serial.begin(115200);
+    Serial1.begin(115200);
+}
+
+char light_c;
+char humidity_c;
+
+void loop()
+{
+    int light = analogRead(lightPin);
+    int humidity = analogRead(humidityPin);
+
+    light = 1024 - light;
+    light_c = (char)map(light, 0, 1024, 0, 100);
+
+    humidity = 1024 - humidity;
+    humidity_c = (char)map(humidity, 0, 1024, 0, 100);
+
+    Serial.print("light = ");
+    Serial.println(light_c);
+
+    Serial.print("humidity = ");
+    Serial.println(humidity_c);
+
+    Serial1.print(light_c);
+    Serial1.print(humidity_c);
+
+    /*short val = ((light & 0xFF) << 8) | (humidity & 0xFF);
+
+    Serial.print("val = ");
+    Serial.println(val, BIN);
+    Serial1.print(val);*/
+
+    delay(2000);
+}
